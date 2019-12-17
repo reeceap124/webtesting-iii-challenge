e9260ef688ad0c18606f68adc2a4cda7edfbc904
Reece Pierson <piersonreece@gmail.com>
2019-12-16 22:41:20 -0600

    test written for controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,8 +4,22 @@ import {render, fireEvent} from '@testing-library/react';
 import Controls from './Controls';
 
 test('control lock button', () => {
-    const {getByText} = render(<Controls locked={false} closed={false}/>)
+    const locked = false
+    const closed = false
+    
+    const {getByText, rerender} = render(<Controls/>)
     const lockButton = getByText(/lock gate/i)
-    fireEvent.click(lockButton);
+    const closeButton = getByText(/close gate/i)
+    expect(lockButton.disabled).toBeTruthy();
+    fireEvent.click(closeButton)
+
+    rerender(<Controls locked={locked} closed={!closed}/>)
+    getByText(/open gate/i)
+    expect(lockButton.disabled).toBeFalsy();
+    fireEvent.click(lockButton)
+
+    rerender(<Controls locked={!locked} closed={!closed}/>)
     getByText(/unlock gate/i)
+    expect(closeButton.disabled).toBeTruthy()
+    fireEvent.click(lockButton)
 }) 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,3 +4,7 @@ import {render} from '@testing-library/react';
 import Dashboard from './Dashboard';
 import Controls from '../controls/Controls';
 import Display from '../display/Display'
+
+test('Dashboard displays', ()=> {
+    render(<Dashboard/>)
+})
